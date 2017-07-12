fcfab7032032d527fb43c2c06d963482b163a373
Robert Chen <robert.chen@thorntechnologies.com>
2017-07-12 18:39:19 -0400

    Names and gear

--- thething.py
+++ thething.py
@@ -1,23 +1,16 @@
-html_doc = """
-<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
-
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
-
-<p class="story">...</p>
-"""
-
 from bs4 import BeautifulSoup
 
 with open("fishy.html") as fp:
-    soup = BeautifulSoup(fp)
+    soup = BeautifulSoup(fp, 'html.parser')
 #soup = BeautifulSoup(html_doc, 'html.parser')
 
-print(soup.prettify())
+#print(soup.prettify())
+aaa = soup.find_all(attrs={"class": "char-portrait-full-link"})
+
+for toon in aaa:
+    print(toon.img['alt']) # toon name
+    print(toon.find(attrs={"class": "char-portrait-full-gear-level"}).string) # gear level
+    print("--")
 
 #print(soup.title)
+#print(aaa)
