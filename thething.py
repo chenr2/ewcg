c43afbef222b2e040e91cbd6fe8cda680e0da481
Robert Chen <robert.chen@thorntechnologies.com>
2017-07-12 18:57:23 -0400

    Gear switch statement

--- thething.py
+++ thething.py
@@ -3,11 +3,38 @@ from bs4 import BeautifulSoup
 with open("fishy.html") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
 
-aaa = soup.find_all(attrs={"class": "char-portrait-full-link"})
+all_toons = soup.find_all(attrs={"class": "char-portrait-full-link"})
 
-for toon in aaa:
+def translate_gear(gear_string):
+    if gear_string == "XI":
+        return 11
+    elif gear_string == "X":
+        return 10
+    elif gear_string == "IX":
+        return 9
+    elif gear_string == "VIII":
+        return 8
+    elif gear_string == "VII":
+        return 7
+    elif gear_string == "VI":
+        return 6
+    elif gear_string == "V":
+        return 5
+    elif gear_string == "IV":
+        return 4
+    elif gear_string == "III":
+        return 3
+    elif gear_string == "II":
+        return 2
+    elif gear_string == "I":
+        return 1
+    else:
+        return 1
+
+for toon in all_toons:
     toon_name = toon.img['alt']
-    print(toon_name)
-    gear_level = toon.find(attrs={"class": "char-portrait-full-gear-level"}).string
-    print(gear_level)
-    print("--")
+    gear_level = translate_gear(toon.find(attrs={"class": "char-portrait-full-gear-level"}).string)
+    if gear_level > 8:
+        print(toon_name)
+        print(gear_level)
+        print("--")
