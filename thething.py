2497f80742e851621c07ee5411e2fa17f0929ac0
Robert Chen <robert.chen@thorntechnologies.com>
2017-07-12 18:45:16 -0400

    variables

--- thething.py
+++ thething.py
@@ -2,15 +2,12 @@ from bs4 import BeautifulSoup
 
 with open("fishy.html") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
-#soup = BeautifulSoup(html_doc, 'html.parser')
 
-#print(soup.prettify())
 aaa = soup.find_all(attrs={"class": "char-portrait-full-link"})
 
 for toon in aaa:
-    print(toon.img['alt']) # toon name
-    print(toon.find(attrs={"class": "char-portrait-full-gear-level"}).string) # gear level
+    toon_name = toon.img['alt']
+    print(toon_name)
+    gear_level = toon.find(attrs={"class": "char-portrait-full-gear-level"}).string
+    print(gear_level)
     print("--")
-
-#print(soup.title)
-#print(aaa)
