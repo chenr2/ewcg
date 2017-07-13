d2e8a9fbe939244d9f17fb76915f0c16c90ea259
Robert Chen <robert.chen@thorntechnologies.com>
2017-07-13 07:32:38 -0400

    Progress

--- 1_extract_members.py
+++ 1_extract_members.py
@@ -16,7 +16,7 @@ base_url = 'https://swgoh.gg'
 for link in all_members:
     member_name = link.string
     clean_name = ''.join(e for e in member_name if e.isalnum())
-    member_url = "wget -O bf/{}.html {}{}".format(clean_name, base_url, link['href'])
+    member_url = "wget -d --header=\"User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11\" -O bf/{}.html {}{}".format(clean_name, base_url, link['href'])
     print(member_url)
 
 
