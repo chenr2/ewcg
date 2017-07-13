bc020a52afea02386b965f21c2f1c46f028f048b
Robert Chen <robert.chen@thorntechnologies.com>
2017-07-12 21:20:24 -0400

    iterate over a few

--- thething.py
+++ thething.py
@@ -1,9 +1,16 @@
-#  wget -O bhartman.html https://swgoh.gg/u/bhartman1002/collection/
+# BF
+
+#  wget -O bf/fishylips.html https://swgoh.gg/u/fishylips/collection/
+#  wget -O bf/bhartman.html https://swgoh.gg/u/bhartman1002/collection/
+#  wget -O bf/xaren.html https://swgoh.gg/u/xarenthariat/collection/
+
+from os import listdir, path
+from os.path import isfile, join
+
+mypath = 'bf'
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 from bs4 import BeautifulSoup
-
-with open("bhartman.html") as fp:
-    soup = BeautifulSoup(fp, 'html.parser')
 
 def translate_gear(gear_string):
     if gear_string == "XI":
@@ -31,13 +38,15 @@ def translate_gear(gear_string):
     else:
         return 1
 
-def get_all_toons():
+def get_all_toons(soup):
     all_toons = soup.find_all(attrs={"class": "char-portrait-full-link"})
     all_toon_tuples = []
     for toon in all_toons:
         toon_name = toon.img['alt']
         gear_level = translate_gear(toon.find(attrs={"class": "char-portrait-full-gear-level"}).string)
-        all_toon_tuples.append((toon_name, gear_level))
+        not_seven_stars = toon.find_all(attrs={"class": "star-inactive"})
+        if not not_seven_stars:
+            all_toon_tuples.append((toon_name, gear_level))
     return all_toon_tuples
 
 def get_haat_squad(roster, haat_squad_set):
@@ -68,30 +77,29 @@ def squad_score(squad):
         score += toon[1]
     return score
 
-roster = get_all_toons()
+for file in onlyfiles:
+    file_path = join(mypath, file)
+    with open(file_path) as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
 
-standard_rebels = get_standard_rebels(roster)
+    roster = get_all_toons(soup)
 
-tiepatine = get_tiepatine(roster)
+        #standard_rebels = get_standard_rebels(roster)
 
-chirpatine = get_chirpapatine(roster)
+    tiepatine = get_tiepatine(roster)
+    chirpatine = get_chirpapatine(roster)
+    teebotine = get_teebotine(roster)
+    palpa_squads = [
+        (tiepatine, squad_score(tiepatine)),
+        (chirpatine, squad_score(chirpatine)),
+        (teebotine, squad_score(teebotine)),
+    ]
+    sorted_by_second = sorted(palpa_squads, key=lambda tup: tup[1], reverse=True)
+    best_palpa = sorted_by_second[0][0]
 
-teebotine = get_teebotine(roster)
-
-palpa_squads = [
-    (tiepatine, squad_score(tiepatine)),
-    (chirpatine, squad_score(chirpatine)),
-    (teebotine, squad_score(teebotine)),
-]
-
-sorted_by_second = sorted(palpa_squads, key=lambda tup: tup[1], reverse=True)
-
-best_palpa = sorted_by_second[0][0]
-
-resistance_p3 = get_resistance_p3(roster)
-
-print(best_palpa)
-print(resistance_p3)
-
+    resistance_p3 = get_resistance_p3(roster)
+    print(path.splitext(file)[0])
+    print(best_palpa)
+    print(resistance_p3)
 
 
