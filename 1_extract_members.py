f390038b09b47edd5be985ac7cb8976a4a150a4e
Robert Chen <robert.chen@thorntechnologies.com>
2017-07-14 19:10:39 -0400

    Generates a folder for each guild

--- 1_extract_members.py
+++ 1_extract_members.py
@@ -1,27 +1,32 @@
-#  wget -O battlefrontiers.html https://swgoh.gg/g/4226/battlefrontiers/
-#  wget -O ewcg.html https://swgoh.gg/g/24217/elegantweaponscivilizedguild/
+#  wget -O guilds/battlefrontiers.html https://swgoh.gg/g/4226/battlefrontiers/
+#  wget -O guilds/ewcg.html https://swgoh.gg/g/24217/elegantweaponscivilizedguild/
 
 from bs4 import BeautifulSoup
 
 import urllib2
+from os import listdir, path
+from os.path import isfile, join
 
-guild_name = "ewcg"
+mypath = 'guilds'
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
-file_path = "{}.html".format(guild_name)
+for file in onlyfiles:
+    guild_name = path.splitext(file)[0]
 
-with open(file_path) as fp:
-    soup = BeautifulSoup(fp, 'html.parser')
+    file_name = "{}.html".format(guild_name)
+    file_path = join(mypath, file_name)
+    print(file_path)
 
-all_members = soup.find('tbody').find_all('a')
+    with open(file_path) as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
 
-base_url = 'https://swgoh.gg'
+    all_members = soup.find('tbody').find_all('a')
 
-for link in all_members:
-    member_name = link.string
-    clean_name = ''.join(e for e in member_name if e.isalnum())
-    member_url = "wget -O {}/{}.html {}{}collection/".format(guild_name, clean_name, base_url, link['href'])
-    print(member_url)
+    base_url = 'https://swgoh.gg'
 
-
-
+    for link in all_members:
+        member_name = link.string
+        clean_name = ''.join(e for e in member_name if e.isalnum())
+        member_url = "wget -O {}/{}.html {}{}collection/".format(guild_name, clean_name, base_url, link['href'])
+        print(member_url)
 
