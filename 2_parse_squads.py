ca837f702353eb99062928baa9c75e8dbf5ff88f
Robert Chen <robert.chen@thorntechnologies.com>
2017-07-24 10:24:54 -0400

    Cleaning up code

--- 2_parse_squads.py
+++ 2_parse_squads.py
@@ -47,9 +47,8 @@ def translate_gear(gear_string):
     else:
         return 1
 
-def get_all_toons(soup):
+def upload_all_toons(soup):
     all_toons = soup.find_all(attrs={"class": "char-portrait-full-link"})
-    all_toon_tuples = []
     for toon in all_toons:
         toon_name = toon.img['alt']
         gear_level_element = toon.find(attrs={"class": "char-portrait-full-gear-level"})
@@ -59,9 +58,7 @@ def get_all_toons(soup):
             gear_level = 1
         not_seven_stars = toon.find_all(attrs={"class": "star-inactive"})
         if not not_seven_stars:
-            all_toon_tuples.append((toon_name, gear_level))
             insert_toon(guild_name, member_name, toon_name, gear_level)
-    return all_toon_tuples
 
 def get_haat_squad(roster, haat_squad_set):
     haat_squad = []
@@ -70,34 +67,11 @@ def get_haat_squad(roster, haat_squad_set):
             haat_squad.append(toon)
     return haat_squad
 
-def get_standard_rebels(roster):
-    return get_haat_squad(roster, {"Princess Leia", "Lando Calrissian", "Biggs Darklighter", "Wedge Antilles", "Admiral Ackbar"})
-
-def get_tiepatine(roster):
-    return get_haat_squad(roster, {"Emperor Palpatine", "TIE Fighter Pilot", "Royal Guard", "Sun Fac", "Stormtrooper Han"})
-
-def get_chirpapatine(roster):
-    return get_haat_squad(roster, {"Emperor Palpatine", "Chief Chirpa", "Royal Guard", "Sun Fac", "Stormtrooper Han"})
-
-def get_teebotine(roster):
-    return get_haat_squad(roster, {"Emperor Palpatine", "Teebo", "Royal Guard", "Sun Fac", "Stormtrooper Han"})
-
-def get_resistance_p3(roster):
-    return get_haat_squad(roster, {"Finn", "Poe Dameron", "R2-D2", "Resistance Trooper", "Resistance Pilot"})
-
-def squad_score(squad):
-    score = 0
-    for toon in squad:
-        score += toon[1]
-    return score
-
-# init db
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('roster') # the toons
 roster_table = dynamodb.Table('member') # the people
 
 root_folder = 'rosters'
-both_guilds_p3_squads = []
 folders = [f for f in listdir(root_folder)]
 for guild_name in folders:
     mypath = "{}/{}".format(root_folder, guild_name)
@@ -112,6 +86,6 @@ for guild_name in folders:
         
         with open(file_path) as fp:
             soup = BeautifulSoup(fp, 'html.parser')
-        roster = get_all_toons(soup)
+        upload_all_toons(soup)
 
 
